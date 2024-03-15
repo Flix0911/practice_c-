98d0411e2c66145bc6acc917c0d1d5dafaff26ff
Erick Valencia <evalencia0911@gmail.com>
2024-03-15 14:10:29 -0600

    changed the Console.WriteLine

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console ~ C# is an older programming language and possibly left over from previous languages
+// When you look at programming, you will see the bedrock
+
+// YOU MUST HAVE ;
+Console.WriteLine("You are a noob");
+
+
+// This is where you're going to write code in
